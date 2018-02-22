3d178345c7b9c539a23eaa63940c0fc7c225d808
Gary <netzver@gmail.com>
2018-02-23 00:30:12 +0200

    add react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,32 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Link, Route, Switch } from 'react-router-dom'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <nav>
+                        <Link to='/'>Home</Link>
+                        <Link to='/demo'>Demo</Link>
+                    </nav>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <div className='content'>
+                    <Switch>
+                        <Route path='/' exact render={() => 'home'} />
+                        <Route path='/demo' render={() => 'demo'} />
+                    </Switch>
+                </div>
+                <footer>
+                    &copy; 2018 - GeekHub JS
+                </footer>
+            </div>
+        );
+    }
 }
 
 export default App;
